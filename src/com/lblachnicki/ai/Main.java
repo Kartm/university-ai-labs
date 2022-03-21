341154b2902f98d40614fa619c761886c4726fc8
Lukasz Blachnicki <lblachnicki@gmail.com>
2022-03-21 19:01:54 +0100

    Initialize project and todo list

---
+++ src/com/lblachnicki/ai/Main.java
@@ -0,0 +1,14 @@
+package com.lblachnicki.ai;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        // 1. load file
+        // 2. construct graph
+        // 3. precalculate heuristics
+        // 4. run A* on all heuristics
+        // 5. benchmark results
+    }
+}
