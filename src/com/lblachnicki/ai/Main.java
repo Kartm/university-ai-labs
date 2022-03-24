3b76dacc01825276760843636027529894ad0132
Lukasz Blachnicki <lblachnicki@gmail.com>
2022-03-24 17:17:47 +0100

    Add test cases and table results

--- src/com/lblachnicki/ai/Main.java
+++ src/com/lblachnicki/ai/Main.java
@@ -49,35 +49,54 @@ public class Main {
     public static void main(String[] args) {
         List<Map.Entry<String, Double>> graphsWithResults = new ArrayList<>();
         graphsWithResults.add(new AbstractMap.SimpleEntry<>("/Users/lblachnicki/Documents/Repositories/university-ai-labs/data/test_from_pdf.dag", 1.97));
+        graphsWithResults.add(new AbstractMap.SimpleEntry<>("/Users/lblachnicki/Documents/Repositories/university-ai-labs/data/test_small.dag", 1.61765274405479));
+        graphsWithResults.add(new AbstractMap.SimpleEntry<>("/Users/lblachnicki/Documents/Repositories/university-ai-labs/data/test_small_sparse.dag", 1.53830736875534));
+        graphsWithResults.add(new AbstractMap.SimpleEntry<>("/Users/lblachnicki/Documents/Repositories/university-ai-labs/data/test_medium.dag", 18.2182804942131));
+        graphsWithResults.add(new AbstractMap.SimpleEntry<>("/Users/lblachnicki/Documents/Repositories/university-ai-labs/data/test_medium_sparse.dag", 15.6808122843504));
+        graphsWithResults.add(new AbstractMap.SimpleEntry<>("/Users/lblachnicki/Documents/Repositories/university-ai-labs/data/test_large.dag", 29.5272732377052));
+        graphsWithResults.add(new AbstractMap.SimpleEntry<>("/Users/lblachnicki/Documents/Repositories/university-ai-labs/data/test_large_sparse.dag", 21.253554970026));
+        graphsWithResults.add(new AbstractMap.SimpleEntry<>("/Users/lblachnicki/Documents/Repositories/university-ai-labs/data/test_xlarge.dag", 39.9794295579195));
+        graphsWithResults.add(new AbstractMap.SimpleEntry<>("/Users/lblachnicki/Documents/Repositories/university-ai-labs/data/test_xlarge_sparse.dag", 29.2775901407003));
+
+        List<String[]> table = new ArrayList<>();
+        table.add(new String[]{"file", "h(0) took ns", "traverse h(0) took ns"});
+        table.add(new String[]{"---------", "---------", "---------"});
+
 
         for (var testCase :
                 graphsWithResults) {
-            List<Node> nodes = GraphLoader.loadFromFile(testCase.getKey());
+            var filePath = testCase.getKey();
+            List<Node> nodes = GraphLoader.loadFromFile(filePath);
+
 
             var artificialParent = nodes.get(nodes.size() - 2);
             var artificialChild = nodes.get(nodes.size() - 1);
 
             long startTime = System.nanoTime();
             artificialChild.BruteForceHeuristic();
-            long estimatedTime = System.nanoTime() - startTime;
-//            System.out.println(estimatedTime);
-            System.out.println(TimeUnit.NANOSECONDS.toMillis(estimatedTime));
-
+            long bruteForceHeuristicTime = System.nanoTime() - startTime;
 
             startTime = System.nanoTime();
             var path = Traverse(artificialParent);
-            estimatedTime = System.nanoTime() - startTime;
-//            System.out.println(estimatedTime);
-            System.out.println(TimeUnit.NANOSECONDS.toMillis(estimatedTime));
-
+            long bruteForceTraverseTime = System.nanoTime() - startTime;
 
             var totalCost = path.stream().mapToDouble(n -> n.getWeight()).sum();
 //            System.out.println(path);
-            System.out.println(totalCost);
+//            System.out.println(totalCost);
 
-            assert(totalCost == testCase.getValue());
+            table.add(new String[]{
+                    filePath.substring(filePath.lastIndexOf("/") + 1),
+//                    String.valueOf(TimeUnit.NANOSECONDS.toMillis(bruteForceHeuristicTime)),
+//                    String.valueOf(TimeUnit.NANOSECONDS.toMillis(bruteForceTraverseTime)),
+                    String.valueOf(bruteForceHeuristicTime),
+                    String.valueOf(bruteForceTraverseTime)
+            });
+
+            assert (testCase.getValue() - totalCost < 0.0000003);
         }
 
-
+        for (var row : table) {
+            System.out.format("%-25s%-15s%-15s\n", row);
+        }
     }
 }
